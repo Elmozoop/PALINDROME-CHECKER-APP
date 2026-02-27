827c170d9d20b866831116094d3cb9f86176218b
elmozap <yashraj2005j@gmail.com>
2026-02-27 09:45:23 +0530

    Added Use Case 8 - Linked List Palindrome Checker

---
+++ src/UseCase8LinkedListPalindromeChecker.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+public class UseCase8LinkedListPalindromeChecker {
+
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node insert(Node head, char data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            return newNode;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        return head;
+    }
+
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow.next);
+        Node firstHalf = head;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== UC8: Linked List Based Palindrome Checker =====");
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        Node head = null;
+
+        for (int i = 0; i < input.length(); i++) {
+            head = insert(head, input.charAt(i));
+        }
+
+        if (isPalindrome(head)) {
+            System.out.println("Result: The given string is a Palindrome.");
+        } else {
+            System.out.println("Result: The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
